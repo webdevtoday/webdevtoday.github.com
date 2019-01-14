04c3e63c1d59bf2591c75427bf3bf324a4517218
webdevtoday <coderwd@gmail.com>
2019-01-14 11:38:06 +0200

    add chat_funcs

--- js/spa.chat.js
+++ js/spa.chat.js
@@ -8,9 +8,10 @@
 	regexp: true, sloppy: true, vars: false,
 	white: true
 */
-/* global $, spa, getComputedStyle */
+/* global $, spa */
 
 spa.chat = (function(){
+	'use strict';
 	//--------- НАЧАЛО ПЕРЕМЕННЫХ В ОБЛАСТИ ВИДИМОСТИ МОДУЛЯ --------
 	var
 		configMap = {
@@ -24,10 +25,20 @@ spa.chat = (function(){
 					+ '</div>'
 					+ '<div class="spa-chat-closer">x</div>'
 					+ '<div class="spa-chat-sizer">'
-						+ '<div class="spa-chat-msgs"></div>'
-						+ '<div class="spa-chat-box">'
-							+ '<input type="text">'
-							+ '<div>send</div>'
+						+ '<div class="spa-chat-list">'
+							+ '<div class="spa-chat-list-box"></div>'
+						+ '</div>'
+						+ '<div class="spa-chat-msg">'
+							+ '<div class="spa-chat-msg-log"></div>'
+							+ '<div class="spa-chat-msg-in">'
+								+ '<form class="spa-chat-msg-form">'
+									+ '<input type="text">'
+									+ '<input type="submit" style="display:none">'
+									+ '<div class="spa-chat-msg-send">'
+										+ 'send'
+									+ '</div>'
+								+ '</form>'
+							+ '</div>'
 						+ '</div>'
 					+ '</div>'
 				+ '</div>',
@@ -66,18 +77,18 @@ spa.chat = (function(){
 			slider_opened_px: 0
 		},
 		jqueryMap = {},
-		setJqueryMap, getEmSize, setPxSizes, setSliderPosition,
-		onClickToggle, configModule, initModule,
+		setJqueryMap, setPxSizes, scrollChat,
+		writeChat, writeAlert, clearChat,
+		setSliderPosition,
+		onTapToggle, onSubmitMsg, onTapList,
+		onSetchatee, onUpdatechat, onListchange,
+		onLogin, onLogout,
+		configModule, initModule,
 		removeSlider, handleResize
 	;
 	//--------- КОНЕЦ ПЕРЕМЕННЫХ В ОБЛАСТИ ВИДИМОСТИ МОДУЛЯ --------
 
 	//----------------- НАЧАЛО СЛУЖЕБНЫХ МЕТОДОВ -------------------
-	getEmSize = function (elem) {
-		return Number(
-			getComputedStyle(elem, '').fontSize.match(/\d*\.?\d*/)[0]
-		);
-	};
 	//------------------ КОНЕЦ СЛУЖЕБНЫХ МЕТОДОВ -------------------
 
 	//-------------------- НАЧАЛО МЕТОДОВ DOM ----------------------
@@ -93,9 +104,13 @@ spa.chat = (function(){
 			$toggle: $slider.find('.spa-chat-head-toggle'),
 			$title: $slider.find('.spa-chat-head-title'),
 			$sizer: $slider.find('.spa-chat-sizer'),
-			$msgs: $slider.find('.spa-chat-msgs'),
-			$box: $slider.find('.spa-chat-box'),
-			$input: $slider.find('.spa-chat-input input[type=text]')
+			$list_box: $slider.find('.spa-chat-list-box'),
+			$msg_log: $slider.find('.spa-chat-msg-log'),
+			$msg_in: $slider.find('.spa-chat-msg-in'),
+			$input: $slider.find('.spa-chat-msg-in input[type=text]'),
+			$send: $slider.find('.spa-chat-msg-send'),
+			$form: $slider.find('.spa-chat-msg-form'),
+			$window: $(window)
 		};
 	};
 	// Конец метода DOM /setJqueryMap/
@@ -103,9 +118,9 @@ spa.chat = (function(){
 	// Начало метода DOM /setPxSizes/
 	setPxSizes = function () {
 		var px_per_em, window_height_em, opened_height_em;
-		px_per_em = getEmSize(jqueryMap.$slider.get(0));
+		px_per_em = spa.util_b.getEmSize(jqueryMap.$slider.get(0));
 		window_height_em = Math.floor(
-			($(window).height() / px_per_em) + 0.5
+			(jqueryMap.$window.height() / px_per_em) + 0.5
 		);
 
 		opened_height_em
@@ -142,8 +157,16 @@ spa.chat = (function(){
 	setSliderPosition = function (position_type, callback) {
 		var height_px, animate_time, slider_title, toggle_text;
 
+		// состояние 'opened' для анонимного пользователя запрещено,
+		// поэтому мы просто возвращаем false; Shell изменит этот
+		// uri и попробует еще раз.
+		if(position_type === 'opened' && configMap.people_model.get_user().get_is_anon()){return false;}
+
 		// вернуть true, если окно чата уже находится в требуемом состоянии
 		if (stateMap.position_type === position_type){
+			if(position_type === 'opened'){
+				jqueryMap.$input.focus();
+			}
 			return true;
 		}
 
@@ -154,6 +177,7 @@ spa.chat = (function(){
 				animate_time = configMap.slider_open_time;
 				slider_title = configMap.slider_opened_title;
 				toggle_text = '=';
+				jqueryMap.$input.focus();
 			break;
 
 			case 'hidden':
@@ -189,10 +213,46 @@ spa.chat = (function(){
 		return true;
 	};
 	// Конец открытого метода DOM /setSliderPosition/
+
+	// Начало закрытых методов DOM для управления областью сообщений чата
+	scrollChat = function(){
+		var $msg_log = jqueryMap.$msg_log;
+		$msg_log.animate(
+			{
+				scrollTop: $msg_log.prop('scrollHeight') - $msg_log.height()
+			},
+			150
+		);
+	};
+
+	writeChat = function(person_name, text, is_user){
+		var msg_class = is_user ? 'spa-chat-msg-log-me' : 'spa-chat-msg-log-msg';
+
+		jqueryMap.$msg_log.append(
+			'<div class="' + msg_class + '">'
+			+ spa.util_b.encodeHtml(person_name) + ': '
+			+ spa.util_b.encodeHtml(text) + '</div>'
+		);
+
+		scrollChat();
+	};
+
+	writeAlert = function(alert_text){
+		jqueryMap.$msg_log.append(
+			'<div class="spa-chat-msg-log-alert">'
+				+ spa.util_b.encodeHtml(alert_text)
+			+ '</div>'
+		);
+
+		scrollChat();
+	};
+
+	clearChat = function(){jqueryMap.$msg_log.empty();};
+	// Конец закрытых методов DOM для управлением областью сообщений чата
 	//--------------------- КОНЕЦ МЕТОДОВ DOM ----------------------
 
 	//---------------- НАЧАЛО ОБРАБОТЧИКОВ СОБЫТИЙ -----------------
-	onClickToggle = function (event) {
+	onTapToggle = function (event) {
 		var set_chat_anchor = configMap.set_chat_anchor;
 		if (stateMap.position_type === 'opened') {
 			set_chat_anchor('closed');
@@ -200,6 +260,127 @@ spa.chat = (function(){
 		else if (stateMap.position_type === 'closed') {
 			set_chat_anchor('opened');
 		} return false;
+	};
+
+	onSubmitMsg = function(event){
+		var msg_text = jqueryMap.$input.val();
+		if(msg_text.trim() === ''){return false;}
+		configMap.chat_model.send_msg(msg_text);
+		jqueryMap.$input.focus();
+		jqueryMap.$send.addClass('spa-x-select');
+		setTimeout(
+			function(){jqueryMap.$send.removeClass('spa-x-select');},
+			250
+		);
+		return false;
+	};
+
+	onTapList = function(event){
+		var $tapped = $(event.elem_target), chatee_id;
+		if(!$tapped.hasClass('spa-chat-list-name')){return false;}
+
+		chatee_id = $tapped.attr('data-id');
+		if(!chatee_id){return false;}
+
+		configMap.chat_model.set_chatee(chatee_id);
+		return false;
+	};
+
+	onSetchatee = function(event, arg_map){
+		var
+			new_chatee = arg_map.new_chatee,
+			old_chatee = arg_map.old_chatee;
+
+		jqueryMap.$input.focus();
+		if(!new_chatee){
+			if(old_chatee){
+				writeAlert(old_chatee.name = ' has left the chat');
+			}
+			else{
+				writeAlert('Your friend has left the chat');
+			}
+			jqueryMap.$title.text('Chat');
+			return false;
+		}
+
+		jqueryMap.$list_box
+			.find('.spa-chat-list-name')
+			.removeClass('spa-x-select')
+			.end()
+			.find('[data-id=' + arg_map.new_chatee.id + ']')
+			.addClass('spa-x-select');
+
+		writeAlert('Now chatting with ' + arg_map.new_chatee.name);
+		jqueryMap.$title.text('Chat with ' + arg_map.new_chatee.name);
+		return true;
+	};
+
+	onListchange = function(event){
+		var
+			list_html = String(),
+			people_db = configMap.people_model.get_db(),
+			chatee = configMap.chat_model.get_chatee();
+
+		people_db().each(function(person, idx){
+			var select_class = '';
+			if(person.get_is_anon() || person.get_is_user()){return true;}
+
+			if(chatee && chatee.id === person.id){
+				select_class = ' spa-x-select';
+			}
+			list_html
+				+= '<div class="spa-chat-list-name'
+				+ select_class + '" data-id="' + person.id + '">'
+				+ spa.util_b.encodeHtml(person.name) + '</div>';
+		});
+
+		if(!list_html){
+			list_html = String()
+				+ '<div class="spa-chat-list-note">'
+				+ 'To chat alone is the fate of all great souls...<br><br>'
+				+ 'No one is online'
+				+ '</div>';
+			clearChat();
+		}
+		// jqueryMap.$list_box.html(list_html);
+		jqueryMap.$list_box.html(list_html);
+	};
+
+	onUpdatechat = function(event, msg_map){
+		var
+			is_user,
+			sender_id = msg_map.sender_id,
+			msg_text = msg_map.msg_text,
+			chatee = configMap.chat_model.get_chatee() || {},
+			sender = configMap.people_model.get_by_cid(sender_id);
+
+		if(!sender){
+			writeAlert(msg_text);
+			return false;
+		}
+
+		is_user = sender.get_is_user();
+
+		if(!(is_user || sender_id === chatee.id)){
+			configMap.chat_model.set_chatee(sender_id);
+		}
+
+		writeChat(sender.name, msg_text, is_user);
+
+		if(is_user){
+			jqueryMap.$input.val('');
+			jqueryMap.$input.focus();
+		}
+	};
+
+	onLogin = function(event, login_user){
+		configMap.set_chat_anchor('opened');
+	};
+
+	onLogout = function(event, logout_user){
+		configMap.set_chat_anchor('closed');
+		jqueryMap.$title.text('Chat');
+		clearChat();
 	};
 	//----------------- КОНЕЦ ОБРАБОТЧИКОВ СОБЫТИЙ -----------------
 
@@ -254,17 +435,31 @@ spa.chat = (function(){
 	// Исключения: нет
 	//
 	initModule = function($append_target){
-		$append_target.append(configMap.main_html);
+		var $list_box;
+
+		// загрузить html чата и кэш jquery
 		stateMap.$append_target = $append_target;
+		$append_target.append(configMap.main_html);
 		setJqueryMap();
 		setPxSizes();
 
 		// установить начальный заголовок и состояние окна чата
 		jqueryMap.$toggle.prop('title', configMap.slider_closed_title);
-		jqueryMap.$head.click(onClickToggle);
 		stateMap.position_type = 'closed';
 
-		return true;
+		// Подписать $list_box на глобальные события jQuery
+		$list_box = jqueryMap.$list_box;
+		$.gevent.subscribe($list_box, 'spa-listchange', onListchange);
+		$.gevent.subscribe($list_box, 'spa-setchatee', onSetchatee);
+		$.gevent.subscribe($list_box, 'spa-updatechat', onUpdatechat);
+		$.gevent.subscribe($list_box, 'spa-login', onLogin);
+		$.gevent.subscribe($list_box, 'spa-logout', onLogout);
+
+		// привязать обработчики событий ввода
+		jqueryMap.$head.bind('utap', onTapToggle);
+		jqueryMap.$list_box.bind('utap', onTapList);
+		jqueryMap.$send.bind('utap', onSubmitMsg);
+		jqueryMap.$form.bind('submit', onSubmitMsg);
 	};
 	// Конец открытого метода /initModule/
 
--- js/spa.model.js
+++ js/spa.model.js
@@ -20,6 +20,7 @@ spa.model = (function(){
 		stateMap = {
 			anon_user: null,
 			cid_serial: 0,
+			is_connected: false,
 			people_cid_map: {},
 			people_db: TAFFY(),
 			user: null
@@ -28,7 +29,7 @@ spa.model = (function(){
 		isFakeData = true,
 
 		personProto, makeCid, clearPeopleDb, completeLogin,
-		makePerson, removePerson, people, initModule
+		makePerson, removePerson, people, chat, initModule
 	;
 
 	// API объекта people
@@ -103,6 +104,7 @@ spa.model = (function(){
 		stateMap.user.id = user_map._id;
 		stateMap.user.css_map = user_map.css_map;
 		stateMap.people_cid_map[user_map._id] = stateMap.user;
+		chat.join();
 
 		// Когда добавится обьект chat, здесь нужно будет войти в чат.
 		$.gevent.publish('spa-login', [stateMap.user]);
@@ -174,11 +176,11 @@ spa.model = (function(){
 		};
 
 		logout = function(){
-			var is_removed, user = stateMap.user;
+			var user = stateMap.user;
 			// Когда добавится обьект chat, здесь нужно будет выйти из чата
-
-			is_removed = removePerson(user);
+			chat._leave();
 			stateMap.user = stateMap.anon_user;
+			clearPeopleDb();
 
 			$.gevent.publish('spa-logout', [user]);
 			return is_removed;
@@ -193,33 +195,227 @@ spa.model = (function(){
 		};
 	}());
 
-	initModule = function(){
-		var i, people_list, person_map;
+	// API объекта chat
+	// ----------------
+	// Объект chat доступен по имени spa.model.chat.
+	// Объект chat предоставляет методы и события для управления
+	// сообщениями в чате. Ниже перечислены его открытые методы:
+	// * join() – войти в чат. Настраивает протокол
+	// взаимодействия чата с сервером, включая публикаторов
+	// глобальных пользовательских событий 'spa-listchange' и
+	// 'spa-updatechat'. Если текущий пользователь анонимный,
+	// то join() возвращает false и больше ничего не делает.
+	// * get_chatee() – вернуть объект person, представляющий
+	// собеседника пользователя. Если такого нет, возвращается
+	// null.
+	// * set_chatee( <person_id> ) – установить в качестве
+	// собеседника объект person с указанным идентификатором
+	// person_id. Если person_id не входит в список людей, то
+	// в качестве собеседника устанавливается null. Если
+	// указанный человек уже является собеседником, то метод
+	// возвращает false. Публикуется глобальное пользовательское
+	// событие 'spa-setchatee'.
+	// * send_msg( <msg_text> ) – отправить сообщение собеседнику.
+	// Публикуется глобальное пользовательское событие
+	// 'spa-updatechat'. Если пользователь анонимный или
+	// собеседник равен null, метод возвращает false и больше
+	// ничего не делает.
+	// * update_avatar( <update_avtr_map> ) – отправить серверу
+	// хэш update_avtr_map. В результате публикуется событие
+	// 'spa-listchange', сопровождаемое обновленным списком
+	// людей и аватаров (атрибут css_map для каждого человека).
+	// Хэш update_avtr_map должен иметь вид
+	// { person_id : person_id, css_map : css_map }.
+	//
+	// Ниже перечислены глобальные пользовательские события
+	// jQuery, публикуемые объектом:
+	// * spa-setchatee – публикуется при установке нового
+	// собеседника. В качестве данных передается хэш вида
+	// { old_chatee : <old_chatee_person_object>,
+	// new_chatee : <new_chatee_person_object>
+	// }
+	// * spa-listchange – публикуется при изменении длины
+	// списка людей в онлайне (то есть когда кто-то входит в чат
+	// или выходит из чата) или его содержимого (то есть когда
+	// изменяется информация о каком-то аватаре).
+	// Получатель этого события должен запросить обновленные
+	// данные у метода people_db объекта модели people.
+	// * spa-updatechat – публикуется при получении или отправке
+	// сообщения. В качестве данных передается хэш вида
+	// { dest_id : <chatee_id>,
+	// dest_name : <chatee_name>,
+	// sender_id : <sender_id>,
+	// msg_text : <message_content>
+	// }
+	//
+	chat = (function(){
+		var
+			_publish_listchange, _publish_updatechat,
+			_update_list, _leave_chat,
 
-		// инициализируем анонимного пользователя
+			get_chatee, join_chat, send_msg,
+			set_chatee, update_avatar,
+
+			chatee = null;
+
+		// Начало внутренних методов
+
+		_update_list = function(arg_list){
+			var i, person_map, make_person_map, person,
+				people_list = arg_list[0],
+				is_chatee_online = false;
+
+			clearPeopleDb();
+
+			PERSON:
+			for(i = 0; i < people_list.length; i++){
+				person_map = people_list[i];
+
+				if(!person_map.name){continue PERSON;}
+				// если пользователь определен, обновить
+				// css_map и больше ничего не делать
+				if(stateMap.user && stateMap.user.id === person_map._id){
+					stateMap.user.css_map = person_map.css_map;
+					continue PERSON;
+				}
+
+				make_person_map = {
+					cid: person_map._id,
+					css_map: person_map.css_map,
+					id: person_map._id,
+					name: person_map.name
+				};
+				person = makePerson(make_person_map);
+
+				if(chatee && chatee.id === make_person_map.id){
+					is_chatee_online = true;
+					chatee = person;
+				}
+			}
+
+			stateMap.people_db.sort('name');
+			// Если собеседник уже не в онлайне, сбросить chatee,
+			// в результате чего возникнет глобальное событие 'spa-setchatee'
+			if(chatee && !is_chatee_online){set_chatee('');}
+		};
+
+		_publish_listchange = function(arg_list){
+			_update_list(arg_list);
+			$.gevent.publish('spa-listchange', [arg_list]);
+		};
+
+		_publish_updatechat = function(arg_list){
+			var msg_map = arg_list[0];
+
+			if(!chatee){set_chatee(msg_map.sender_id);}
+			else if(msg_map.sender_id !== stateMap.user.id && msg_map.sender_id !== chatee.id){set_chatee(msg_map.sender_id);}
+
+			$.gevent.publish('spa-updatechat', [msg_map]);
+		};
+		// Конец внутренних методов
+
+		_leave_chat = function(){
+			var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+			chatee = null;
+			stateMap.is_connected = false;
+			if(sio){sio.emit('leavechat');}
+		};
+
+		get_chatee = function(){return chatee;};
+
+		join_chat = function(){
+			var sio;
+			if(stateMap.is_connected){return false;}
+
+			if(stateMap.user.get_is_anon()){
+				console.warn('User must be defined before joining chat');
+				return false;
+			}
+
+			sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+			sio.on('listchange', _publish_listchange);
+			sio.on('updatechat', _publish_updatechat);
+			stateMap.is_connected = true;
+			return true;
+		};
+
+		send_msg = function(msg_text){
+			var msg_map,
+				sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+
+			if(!sio){return false;}
+			if(!(stateMap.user && chatee)){return false;}
+
+			msg_map = {
+				dest_id: chatee.id,
+				dest_name: chatee.name,
+				sender_id: stateMap.user.id,
+				msg_text: msg_text
+			};
+
+			// мы опубликовали updatechat, чтобы можно было показать
+			// текущий список сообщений
+			_publish_updatechat([msg_map]);
+			sio.emit('updatechat', msg_map);
+			return true;
+		};
+
+		set_chatee = function(person_id){
+			var new_chatee;
+			new_chatee = stateMap.people_cid_map[person_id];
+			if(new_chatee){
+				if(chatee && chatee.id === new_chatee.id){
+					return false;
+				}
+			}
+			else{
+				new_chatee = null;
+			}
+
+			$.gevent.publish('spa-setchatee',
+				{old_chatee: chatee, new_chatee: new_chatee}
+			);
+			chatee = new_chatee;
+			return true;
+		};
+
+		// avatar_update_map должен иметь вид:
+		// { person_id: <string>, css_map: {
+		// 	top: <int>, left: <int>,
+		// 	'background-color': <string>
+		// }}
+		//
+		update_avatar = function(avatar_update_map){
+			var sio = isFakeData ? spa.fake.mockSio : spa.data.getSio();
+			if(sio){
+				sio.emit('updateavatar', avatar_update_map);
+			}
+		};
+
+		return {
+			_leave: _leave_chat,
+			get_chatee: get_chatee,
+			join: join_chat,
+			send_msg: send_msg,
+			set_chatee: set_chatee,
+			update_avatar: update_avatar
+		};
+	}());
+
+	initModule = function(){
+		// инициализировать анонимный обьект person
 		stateMap.anon_user = makePerson({
 			cid: configMap.anon_id,
 			id: configMap.anon_id,
 			name: 'anonymous'
 		});
-		stateMap.user = stateMap.anon_user;
 
-		if(isFakeData){
-			people_list = spa.fake.getPeopleList();
-			for(i = 0; i < people_list.length; i++){
-				person_map = people_list[i];
-				makePerson({
-					cid: person_map._id,
-					css_map: person_map.css_map,
-					id: person_map._id,
-					name: person_map.name
-				});
-			}
-		}
+		stateMap.user = stateMap.anon_user;
 	};
 
 	return {
 		initModule: initModule,
+		chat: chat,
 		people: people
 	};
 }());
